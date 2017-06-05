f715874ea5d553afbb3c266bb2cc35b883719308
19julio94 <juliopatriciodasilva@gmail.com>
2017-06-05 21:54:25 +0200

    fix errors

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,5 +1,4 @@
 import javax.swing.*;
-import java.util.Scanner;
 
 /**
  * Created by BetweenBinaryCode on 05/06/2017.
@@ -8,13 +7,8 @@ public class HelloWorld {
 
     public static void main(String[] args) {
 
-        //System.out.println("Hola bienvenidos a BetweenBinaryCode ");
-        Scanner sc = new Scanner(System.in);
-
-        String z= sc.nextLine();
-
-        System.out.println("La palabra introducida es :" + z);
-        JOptionPane.showMessageDialog(null,z);
+        System.out.println("Hola bienvenidos a BetweenBinaryCode ");
     }
+
 
 }
